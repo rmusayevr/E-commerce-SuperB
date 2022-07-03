63e21e29a75c882267fb4c92db22e2a6d55898a7
rmusayevr <rmusayevr19@gmail.com>
2022-07-03 17:43:50 +0400

    Static and Template Files

--- Product/views.py
+++ Product/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 def product_detail(request):
-    return render(request, "product_detail.html")
+    return render(request, "product-detail.html")
 
 def product_list(request):
-    return render(request, "product_list.html")
+    return render(request, "product-list.html")
 
 def quick_view(request):
     return render(request, "quick_view.html")
